0f5e01a36ac50a198ecf7b81724764a28547bcd8
Zeytoun1 <husamkassab02@gmail.com>
2025-04-09 02:39:08 -0400

    config changes

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -4,6 +4,10 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
+const basePath = publicRuntimeConfig.basePath || "";
 
 const RecentProjects = () => {
   return (
@@ -24,10 +28,10 @@ const RecentProjects = () => {
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
-                  <img src="/bg.png" alt="bgimg" />
+                  <img src={`${basePath}/bg.png`} alt="bgimg" />
                 </div>
                 <img
-                  src={item.img}
+                  src={`${basePath}${item.img}`}
                   alt="cover"
                   className="z-10 absolute bottom-0"
                 />
@@ -57,14 +61,18 @@ const RecentProjects = () => {
                         transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
-                      <img src={icon} alt="icon5" className="p-2" />
+                      <img
+                        src={`${basePath}${icon}`}
+                        alt="icon5"
+                        className="p-2"
+                      />
                     </div>
                   ))}
                 </div>
 
                 <div className="flex justify-center items-center">
                   <a
-                    href={item.link} // Ensure `item.link` contains the correct URL
+                    href={item.link}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="flex lg:text-xl md:text-xs text-sm text-purple"
