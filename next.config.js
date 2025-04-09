aaf0c433322f15b27e22376c8f8c26b678ac9b29
Zeytoun1 <husamkassab02@gmail.com>
2025-04-09 02:26:05 -0400

    config changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = (process.env.NODE_ENV = "production");
 const nextConfig = {
+  basePath: isProd ? "/personal-portfolio" : "",
+  assetPrefix: isProd ? "/personal-portfolio/" : "",
+  trailingSlash: true,
   output: "export",
   distDir: "dist",
   images: {
     unoptimized: true,
-  }
+  },
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
