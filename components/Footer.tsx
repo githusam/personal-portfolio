07e556c434a93a25f241a886b2565291c7cdef3c
Zeytoun1 <husamkassab02@gmail.com>
2025-04-09 03:55:12 -0400

    bug fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -49,7 +49,7 @@ const Footer = () => {
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <img
-                src={`${basePath}/{info.img}`}
+                src={`${basePath}/${info.img}`}
                 alt="social-icon"
                 width={20}
                 height={20}
