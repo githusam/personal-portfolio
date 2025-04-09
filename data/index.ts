018b495256aea03435249224caa923a4331ecb3d
Zeytoun1 <husamkassab02@gmail.com>
2025-04-09 03:29:38 -0400

    config changes

--- data/index.ts
+++ data/index.ts
@@ -6,16 +6,18 @@ export const navItems = [
   { name: "Contact", link: "#contact" },
 ];
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 export const gridItems = [
   {
     id: 1,
     title:
-      "Code is only as strong as the teamwork behind it—clarity and collaboration come first.",
+      "Code is only as strong as the teamwork behind it — clarity and collaboration come first.",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: `${basePath}/b1.svg`,
     spareImg: "",
   },
   {
@@ -45,8 +47,8 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
+    img: `${basePath}/grid.svg`,
+    spareImg: `${basePath}/b4.svg`,
   },
 
   {
@@ -56,8 +58,8 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
+    img: `${basePath}/b5.svg`,
+    spareImg: `${basePath}/grid.svg`,
   },
   {
     id: 6,
@@ -76,7 +78,7 @@ export const projects = [
     id: 1,
     title: "Large Language Model Analyzer",
     des: "A comprehensive tool for analyzing and visualizing large language models.",
-    img: "/p1.png",
+    img: `${basePath}/p1.png`,
     iconLists: [
       "/re.svg",
       "/tail.svg",
@@ -90,7 +92,7 @@ export const projects = [
     id: 2,
     title: "TMUsic - Music Social App",
     des: "A social media platform for music lovers to connect and share their tracks.",
-    img: "/p2.png",
+    img: `${basePath}/p2.png`,
     iconLists: ["re.svg", "php.svg", "tail.svg", "ts.svg", "/sql.png"],
     link: "https://github.com/vijaysatchi/tmu-music",
   },
@@ -98,7 +100,7 @@ export const projects = [
     id: 3,
     title: "Greenspace Management System",
     des: "A comprehensive urban management system for greenspaces.",
-    img: "/p3.png",
+    img: `${basePath}/p3.png`,
     iconLists: [
       "/re.svg",
       "/javascript.png",
@@ -112,7 +114,7 @@ export const projects = [
     id: 4,
     title: "Weather Application",
     des: "A simple weather application that provides real-time weather updates.",
-    img: "/p4.png",
+    img: `${basePath}/p4.png`,
     iconLists: ["/re.svg", "/javascript.png", "/node.png"],
     link: "https://github.com/githusam/weather-app",
   },
